6074a63bc2950c2e46be5d4af2edaab30964cbd5
Dereck <decknho26@gmail.com>
2024-04-28 14:58:55 -0300

    Create Aula.py

---
+++ Python/Aula.py
@@ -0,0 +1,8 @@
+from random import randint
+x = randint(0, 100)
+y = randint(0, 100)
+
+if x == y:
+    print(f'As variaveis são {x} e {y}, portando são iguais igual!')
+else:
+    print(f'As variaveis são {x} e {y}, portanto são diferentes!')
