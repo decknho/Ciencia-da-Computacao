c7b378299c4c0a9efa12bf70f28fa0bf53a4ad5c
Dereck <decknho26@gmail.com>
2024-05-04 15:00:34 -0300

    Update Questão2.py

--- Python/Trabalho/Questão2.py
+++ Python/Trabalho/Questão2.py
@@ -1,3 +1,15 @@
 print('Bem-vindo(a) a loja do Dereck Eder')
 
+valor_total = 0
+
 """Cardápio"""
+
+
+"""Mensagem de erro em caso de sabor invalido"""
+sabor = input('Escolha o sabor (CP/AC) :').upper
+while ('AC, CP') not in sabor:
+    print('Sabor invalido. selecione novamente!')
+    sabor = input('Escolha o sabor (CP/AC) :').upper
+
+
+"""Mensagem de erro em caso de tamanho invalido"""
