37065b05bc2d7b9b2a2a469f1d105397500965a0
Dereck <decknho26@gmail.com>
2024-05-20 01:13:21 -0300

    FINAL

--- Python/Aula.py
+++
@@ -1,7 +0,0 @@
-livros = []
-livro = {'id': [], 'nome' : [], 'autor' : [], 'editora' : []}
-livro['nome'] = input('Nome do livro? ')
-livro['autor'] = input('Nome do autor? ')
-livro['editora'] = input('Nome da editora? ')
-livros.append(livro)
-
