0c36e3872d4c4cf5a975269dbeb528c1410baf9e
Dereck <decknho26@gmail.com>
2024-08-10 15:44:42 -0300

    sla o que ro fazendo

--- Python/sla.py
+++ Python/sla.py
@@ -1,6 +1,4 @@
-palavra = str("Dereck")
-num = len(palavra)
-lista = []
-for c in range(0, num):
-    print(f'{palavra[c]}', end=' ')
-    
+contatos = [{'Yan': '1234-5678'}, {'Pedro': '9999-9999'}, {'Ana': '8765-4321'},
+  {'Marina': '8877-7788'}, {'João': '8887-7778'}]
+for c in range(0, 5):
+    print(contatos[c])
