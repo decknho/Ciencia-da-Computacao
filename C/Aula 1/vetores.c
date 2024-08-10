9db14411e0e5a74863849589ae18575de8b83c6c
Dereck <decknho26@gmail.com>
2024-08-10 15:26:23 -0300

    eu sla po

--- C/Aula 1/vetores.c
+++ C/Aula 1/vetores.c
@@ -9,18 +9,25 @@ int main() {
     for (i = 0; i < 10; i++)
     {
         posicao = i + 1;
-        numero[i] = i;
-        printf_s("Indice: %i = Valor: %d\n",posicao, numero[i]);
+        numero[i] = i + 1;
+        printf_s("Indice %d, Valor %d\n",posicao, numero[i]);
     }
 
-    printf_s("Digite o indice do valor: ");
-    scanf("%d", indice);
+    printf_s("Escolha o indice: ");
+    scanf("%d", &indice);
+    indice--;
 
-
-    printf_s("Digite o indice do valor: ");
-    scanf("%d", novovalor);
+    printf_s("Digite o novo valor para o indice: ");
+    scanf("%d", &novovalor);
 
     numero[indice] = novovalor;
+
+    for (i = 0; i < 10; i++)
+    {
+        posicao = i + 1;
+        printf_s("Indice %d, Valor %d\n",posicao, numero[i]);
+    }
+
     
     
     return 0;
