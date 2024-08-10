0ef87ce5ee7d89cc1c85ce60a70b568a63207e4e
Dereck <decknho26@gmail.com>
2024-08-10 14:13:54 -0300

    ta

---
+++ C/Aula 1/vetores.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main() {
+    setlocale(LC_ALL, "Portuguese");
+    int numero[10]
+    int i, indice, novovalor;
+    
+    return 0;
+}
---
+++ Python/sla.py
@@ -0,0 +1,6 @@
+palavra = str("Dereck")
+num = len(palavra)
+lista = []
+for c in range(0, num):
+    print(f'{palavra[c]}', end=' ')
+    
