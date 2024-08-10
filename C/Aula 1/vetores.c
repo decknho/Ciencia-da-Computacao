1129888b79594fe6dc2247a3d63be616068f70db
Dereck <decknho26@gmail.com>
2024-08-10 15:08:37 -0300

    estudando mais

--- C/Aula 1/vetores.c
+++ C/Aula 1/vetores.c
@@ -3,8 +3,16 @@
 
 int main() {
     setlocale(LC_ALL, "Portuguese");
-    int numero[10]
-    int i, indice, novovalor;
+    int numero[10];
+    int i, posicao, indice, novovalor;
+
+    for (i = 0; i < 10; i++)
+    {
+        posicao = i + 1;
+        numero[i] = i;
+        printf_s("%i = %d\n",posicao, numero[i]);
+    }
+    
     
     return 0;
 }
