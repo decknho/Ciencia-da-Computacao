8c5d64ad407557b95878d0a51c01462466e9d5f5
Dereck <decknho26@gmail.com>
2024-09-03 21:52:55 -0300

    é

--- C/Aula 3/sla.c
+++ C/Aula 3/sla.c
@@ -3,9 +3,9 @@
 
 int main() {
     setlocale(LC_ALL, "Portuguese");
-    int x = 10;
-    int *p_x = &x;
-    printf_s("%i", &p_x);
+    char str;
+    scanf_s("%s", str);
+    printf("Nova string: %s\n", str);
 
     return 0;
 }
